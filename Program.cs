555dea5f41f65146011370943ec1250dad95902d
PetterBostrom <petter.bostrom@gmail.com>
2023-04-07 10:48:26 +0200

    Added lines for readability

--- Program.cs
+++ Program.cs
@@ -13,10 +13,14 @@
             boxCollection.Add(new Box(10, 15, 10));
             Display(boxCollection);
 
+            Console.WriteLine("--------------------------------------");
+
             // Remove a box from the list.
             Box toRemove = new Box(5,8,10);
             boxCollection.Remove(toRemove);
             Display(boxCollection);
+
+            Console.WriteLine("--------------------------------------");
 
             // Add it again and check if it's in the collection.
             boxCollection.Add(toRemove);
@@ -29,8 +33,24 @@
                 Console.WriteLine("The box is not in the list.");
             }
 
+            Console.WriteLine("--------------------------------------");
+
+            // Check if another box is in the collection
+            if (boxCollection.Contains(new Box(3,3,3)))
+            {
+                Console.WriteLine("The box exists in the collection.");
+            }
+            else
+            {
+                Console.WriteLine("The box is not in the list.");
+            }
+
+            Console.WriteLine("--------------------------------------");
+
             // Test if Add method prevents box with same dimensions being added.
             boxCollection.Add(new Box(2, 7, 7));
+
+            Console.WriteLine("--------------------------------------");
 
             // Test if Add method prevents box with same volume being added.
             boxCollection.Add(new Box(7, 2, 7));
